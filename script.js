041a52d9888e60a740ff9cba35125c06d69825c6
atticusa1149 <the13mar@gmail.com>
2025-05-11 15:11:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const checkInput = () => {
     let userInput = document.getElementById('inpuut').value.trim().toLowerCase();
     if (userInput === "happy mothers day 10") {
-        document.getElementById('GoodJob').innerText = 'good job';
+        document.getElementById('GoodJob').innerText = 'WOW! you got this far, before we continue, happy mothers day, but heres the riddle: a piece of styro-foam taped to
+        the wall 💲.';
     }
 };
